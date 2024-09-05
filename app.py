1129f7d29e2e87b3a7a553b6af2a0ac53e414c8d
Jedrzej.me <50214379+jedrzejme@users.noreply.github.com>
2024-09-05 20:58:14 +0200

    Change backup filename from ShortenThis to "app_name" [5.4.3]

--- app.py
+++ app.py
@@ -251,7 +251,7 @@ def control_panel_action():
         return redirect('/control-panel?section=users')
     elif action == 'download_backup' and session.get('auth'):
         now = datetime.now()
-        return send_file("database.db", as_attachment=True, download_name=f"ShortenThis-backup-{now.strftime("%Y-%m-%d %H-%M-%S")}.db")
+        return send_file("database.db", as_attachment=True, download_name=f"""{getting_config("app_name")}-backup-{now.strftime("%Y-%m-%d %H-%M-%S")}.db""")
     else:
         return abort(404, description="Action not found")
 
